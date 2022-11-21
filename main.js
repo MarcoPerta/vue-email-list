8280bcb04d8a5c46a33a6ec7aba81db648b71461
MarcoPerta <perta.marco@gmail.com>
2022-11-21 15:59:35 +0100

    non risp html

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 var app = new Vue({
     el: '#app',
     data: {
-      
+      emailList : '',
     },
 
     mounted() {
 
+            // for(let i=0; i<10 ; i++){
+            //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            //     .then( (response) => {
+            //     let email = response.data.response;
+            //     this.emailList = email;
+            // })
     },
  
     methods: {
-  
+
+        inizia(){
+
+            for(let i=0; i<10 ; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => {
+                let email = response.data.response;
+                this.emailList = email;
+                console.log(this.emailList)
+            })}
+        }
     }
   })
